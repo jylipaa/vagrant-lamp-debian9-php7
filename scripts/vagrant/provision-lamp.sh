4b3651e9e58750087c15ee02ffd74ef3083784b3
Jani Yli-Paavola <jylipaa@iki.fi>
2017-06-27 13:38:52 +0300

    enabled mod_rewrite

--- scripts/vagrant/provision-lamp.sh
+++ scripts/vagrant/provision-lamp.sh
@@ -5,4 +5,5 @@ sudo apt install apache2 php7.0 mariadb-server php7.0-mysql mariadb-client php-x
 sudo a2dissite 000-default
 sudo cp /vagrant/scripts/vagrant/conf/apache-lamp.conf /etc/apache2/sites-available/
 sudo a2ensite apache-lamp
+sudo a2enmod rewrite
 sudo systemctl reload apache2
