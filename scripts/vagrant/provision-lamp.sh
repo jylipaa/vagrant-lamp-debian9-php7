8b9a3d77ab5fedff8852ae21a50597459ff0e03c
Jani Yli-Paavola <jylipaa@users.noreply.github.com>
2017-07-14 17:20:05 +0300

    Added php7.0-mbstring

--- scripts/vagrant/provision-lamp.sh
+++ scripts/vagrant/provision-lamp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt upgrade -y
-sudo apt install apache2 php7.0 mariadb-server php7.0-mysql mariadb-client php-xdebug -y
+sudo apt install apache2 php7.0 mariadb-server php7.0-mysql mariadb-client php-xdebug php7.0-mbstring -y
 sudo a2dissite 000-default
 sudo cp /vagrant/scripts/vagrant/conf/apache-lamp.conf /etc/apache2/sites-available/
 sudo a2ensite apache-lamp
